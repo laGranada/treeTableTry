5cceab02d2cb743fa86bf3b1bab9faa0f12f07ac
MaggieSaurusRex <perfection_in_progress@live.de>
2019-02-08 15:09:12 +0100

    Add files via upload

---
+++ treeTableTest/src/main/java/com/mycompany/treetabletest/Document.java
@@ -0,0 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.treetabletest;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+
+/**
+ *
+ * @author margarita.dueck
+ */
+public class Document implements Serializable, Comparable<Document> {
+ 
+    private String name;
+     
+    private String size;
+     
+    private String type;
+     
+    public Document(String name, String size, String type) {
+        this.name = name;
+        this.size = size;
+        this.type = type;
+    }
+ 
+    public String getName() {
+        return name;
+    }
+ 
+    public void setName(String name) {
+        this.name = name;
+    }
+ 
+    public String getSize() {
+        return size;
+    }
+ 
+    public void setSize(String size) {
+        this.size = size;
+    }
+ 
+    public String getType() {
+        return type;
+    }
+ 
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 71 * hash + Objects.hashCode(this.name);
+        hash = 71 * hash + Objects.hashCode(this.size);
+        hash = 71 * hash + Objects.hashCode(this.type);
+        return hash;
+    }
+ 
+    
+ 
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Document other = (Document) obj;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        if (size == null) {
+            if (other.size != null)
+                return false;
+        } else if (!size.equals(other.size))
+            return false;
+        if (type == null) {
+            if (other.type != null)
+                return false;
+        } else if (!type.equals(other.type))
+            return false;
+        return true;
+    }
+ 
+    @Override
+    public String toString() {
+        return name;
+    }
+ 
+    public int compareTo(Document document) {
+        return this.getName().compareTo(document.getName());
+    }
+}
